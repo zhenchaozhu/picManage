851ba842c20cde837561102f4bf9f8e85aa34484
zhenchaozhu <zhenchaozhu@sohu-inc.com>
2013-01-30 18:40:24 +0800

    添加主页面

--- pic_manage/urls.py
+++ pic_manage/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from pic_manage.picture.views import homepage_view
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -16,3 +17,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()
