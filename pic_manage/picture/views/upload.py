b302380300fc53a9a76b5a09fd43bf70eb0c4f5b
zhenchaozhu <zhenchaozhu@sohu-inc.com>
2013-02-01 19:22:48 +0800

    修改代码结构

---
+++ pic_manage/picture/views/upload.py
@@ -0,0 +1,4 @@
+def upload_files(request):
+    print 1
+    files = request.Files
+    print files
--- pic_manage/urls.py
+++ pic_manage/urls.py
@@ -2,13 +2,15 @@ from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from pic_manage.picture.views import homepage_view
+from pic_manage.picture.views.main import homepage_view
+from pic_manage.picture.views.upload import upload_files
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', homepage_view),
+    url(r'^/upload/', upload_files),
     # url(r'^pic_manage/', include('pic_manage.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
