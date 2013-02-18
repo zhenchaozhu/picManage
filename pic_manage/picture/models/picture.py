90d6cd08b715239626fc86203a04d7615a54e89e
zhenchaozhu <zhenchaozhu@sohu-inc.com>
2013-02-18 17:35:30 +0800

    add

--- pic_manage/picture/models/picture.py
+++ pic_manage/picture/models/picture.py
@@ -6,9 +6,15 @@ class Picture(models.Model):
 
     @staticmethod
     def get_path(page_num):
-        start = page_num * 10
-        end = start + 10
-        return [obj.path for obj in Picture.objects.all()[start: end]]
+        start = page_num * 8
+        end = start + 8
+        return [obj.path for obj in Picture.objects.order_by('-id')[start: end]]
+
+    @staticmethod
+    def save_img(create_time, path):
+        pic = Picture(create_date = create_time, path = path)
+        pic.save()
+
 
     class Meta(object):
         app_label = 'picture'
--- pic_manage/picture/views/upload.py
+++ pic_manage/picture/views/upload.py
@@ -4,8 +4,9 @@ import time
 import datetime
 import MySQLdb as mdb
 from django.http import HttpResponse
+from pic_manage.settings import ROOT_PATH
 from pic_manage.settings import UPLOAD_FILE_ROOT
-
+from pic_manage.picture.models import Picture
 def upload_files(request):
     files = request.FILES
     context = {
@@ -29,23 +30,13 @@ def get_file_paths(files):
     return file_paths
 
 def save_file(file_path, file):
+    if not os.path.exists(UPLOAD_FILE_ROOT):
+        os.mkdir(UPLOAD_FILE_ROOT)
     if not os.path.exists(file_path):
         des = open(file_path, 'wb+')
         for chunk in file.chunks():
             des.write(chunk)
         des.close()
         create_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        save_file_path(create_time, file_path)
+        Picture.save_img(create_time, file_path.replace(ROOT_PATH, ''))
         return True
-
-def save_file_path(create_time, file_path):
-    con = None
-    try:
-        con = mdb.connect('localhost', 'root', 'zzc', 'picture')
-        cur = con.cursor()
-        sql = 'insert into picture_picture(create_date, path) values("%s", "%s")'
-        cur.execute(sql % (create_time, file_path))
-        con.commit()
-    finally:
-        if con:
-            con.close()
